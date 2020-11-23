61d87cb0f0cf23ca7ccc0a58eee2400d61a71774
AmdyBFB <sawareamdy541@gmail.com>
2020-11-23 19:27:55 +0100

    implementation de quelques methodes et ses tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,29 @@
 public class Calculator {
+
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        if(b == 0)throw new ArithmeticException();
+        else
+            return a/b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b ;
+    }
+
+    public int min(int a, int b){
+        return Math.min(a,b);
+    }
+
+    public int max(int a, int b){
+        return Math.max(a,b);
+    }
 }
